948a8cd5f50bde3784ce6f42490a99ebc5292c4f
NM-90 <111452227+NM-90@users.noreply.github.com>
2022-12-19 11:58:03 -0600

    Add files via upload

---
+++ app3.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+
+
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+
+app = Flask(__name__)
+
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+      filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}   
+   return jsonify(precip)
